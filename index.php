58a267770a4656ba9cd7b8b1625c497c5c7a6a06
Timothy Gibbons <tjgibbons10@gmail.com>
2015-10-27 07:45:04 -0500

    update index.php
    Design layout...

--- index.php
+++ index.php
@@ -4,6 +4,24 @@ include path.'inc/init.php';
 $user = new User();
 if(!$user->isLoggedIn()){
 	Redirect::to(path.'pages/login');
-}else{
 }
 ?>
+<html>
+	<head>
+		<?php include path.'assets/head.php'?>
+	</head>
+	<body>
+		<?php include path.'assets/nav.php'?>
+		<div class="container">
+			<div class="row">
+				<div class="jumbotron">
+					<h1><?php Setting::show('title')?></h1>	
+				</div>
+			</div>
+			<div class="">
+			
+			</div>
+		</div>
+		<?php include path.'assets/foot.php';?>
+	</body>
+</html>
